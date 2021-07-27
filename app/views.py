d5b0368546d1a5f0a1565a61e55de2d17b6a29ea
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-07-27 12:29:40 +0300

    index view created

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.models import User
+from .forms import PostForm
+from .models import Post
+
 
 # Create your views here.
+def welcome(request):
+    posts = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    form = PostForm(request.POST or None, files=request.FILES)      
+    if form.is_valid():
+        post=form.save(commit=False)
+        post.user = request.user.profile
+        post.save()
+        return redirect('home')
+    context = {
+        'posts': posts,
+        'form': form,
+        'users':users,
+    }
+    # users = Profile.objects.all()
+    return render(request, 'index.html',context)
+
--- yelp/settings.py
+++ yelp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -70,14 +72,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yelp.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yelp',
+        'USER': 'hamisi',
+    'PASSWORD':'1995',
     }
 }
 
@@ -124,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
